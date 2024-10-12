137b07a06907aac1a2d7cd1df5e99d8ba78a4b39
pietrodev07 <pietro.dev.07@gmail.com>
2024-10-12 10:36:37 +0200

    feat(sorting): add insertion-sort algorithm

---
+++ sorting/insertion_sort.go
@@ -0,0 +1,14 @@
+package sorting
+
+// InsertionSort implements the insertion sort algorithm.
+func InsertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+}
