c52d51a78aa8fd4cde63a16fc4a5e90c13f1cf0f
pietrodev07 <pietro.dev.07@gmail.com>
2024-10-12 10:37:07 +0200

    feat(sorting): add selection-sort algorithm

---
+++ sorting/selection_sort.go
@@ -0,0 +1,14 @@
+package sorting
+
+// SelectionSort implements the selection sort algorithm.
+func SelectionSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		minIdx := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+		arr[i], arr[minIdx] = arr[minIdx], arr[i]
+	}
+}
